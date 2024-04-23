ff34a4d1a7950e203df9c7a0acfe6b7580fe8cfe
LuigiMottola <sputnick427@gmail.com>
2024-04-23 16:38:40 +0200

    Termine esercizio

---
+++ src/UnicodeFinder.java
@@ -0,0 +1,16 @@
+public class UnicodeFinder {
+
+    public StringBuilder unicodeAtIndex(String myString, int index) {
+        StringBuilder result = new StringBuilder();
+
+        char character = myString.charAt(index);
+
+        result.append("Il carattere al indice ")
+                .append(index)
+                .append(" Ã¨: ")
+                .append("\\u") // u unicode
+                .append(String.format("%04x", (int)character)); //%04x formatta esadecimale
+        return result;
+    }
+
+}
